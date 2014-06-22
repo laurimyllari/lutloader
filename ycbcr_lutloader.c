01699f899893422de47949653b68d932d1638184
Lauri Myllari <lauri.myllari@movial.com>
2014-06-21 21:11:52 -0700

    Read 3DLUT data

--- ycbcr_lutloader.c
+++ ycbcr_lutloader.c
@@ -1,9 +1,54 @@
 #include "lcms2.h"
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
 
 // borrow some lcms2 internals - probably a bad idea?
 extern cmsToneCurve** _cmsStageGetPtrToCurveSet(const cmsStage* mpe);
+
+typedef struct
+{
+    cmsUInt16Number prevval[3];
+    int valcount[3];
+} countcargo_t;
+
+cmsInt32Number lutSizeCounter(register const cmsUInt16Number In[],
+        register cmsUInt16Number Out[],
+        register void * Cargo)
+{
+    countcargo_t *c = (countcargo_t*)Cargo;
+
+    for (int d = 0; d < 3; d++)
+    {
+        if (In[d] > c->prevval[d])
+        {
+            c->prevval[d] = In[d];
+            c->valcount[d]++;
+        }
+    }
+    return 1;
+}
+
+typedef struct
+{
+    cmsFloat32Number *lut;  // lut output data
+    int s1, s2, s3;         // lut dimensions
+} fillcargo_t;
+
+cmsInt32Number lutFiller(register const cmsFloat32Number In[],
+        register cmsFloat32Number Out[],
+        register void * Cargo)
+{
+    fillcargo_t *f = (fillcargo_t*)Cargo;
+    int x = In[0] * f->s1;
+    int y = In[1] * f->s2;
+    int z = In[2] * f->s3;
+    int index = 3 * (z*f->s2 + y*f->s1 + x);
+    f->lut[index] = Out[0];
+    f->lut[index+1] = Out[1];
+    f->lut[index+2] = Out[2];
+    return 1;
+}
 
 int main(int  argc, char* argv[])
 {
@@ -11,8 +56,12 @@ int main(int  argc, char* argv[])
     cmsPipeline *pipeline;
     cmsStage *inputstage, *clutstage, *outputstage;
     cmsToneCurve **inputcurves, **outputcurves;
-    float *outputtable[3];
+    countcargo_t countcargo;
     int outputtablesize;
+    float *outputtable[3];
+    fillcargo_t fillcargo;
+    int lutsize[3];
+    float *lut;
 
     if (argc != 2) {
         printf("ycbcr_lotloader <YCbCr->RGB icc device link>\n");
@@ -111,10 +160,29 @@ int main(int  argc, char* argv[])
     }
 #endif
 
+    // this is a little convoluted, but lcms2 doesn't give us the LUT size?
+    memset(&countcargo, 0, sizeof(countcargo_t));
+    cmsStageSampleCLut16bit(clutstage, lutSizeCounter, &countcargo, SAMPLER_INSPECT);
+    lutsize[0] = countcargo.valcount[0]; lutsize[1] = countcargo.valcount[1]; lutsize[2] = countcargo.valcount[2];
+    printf("lut size %dx%dx%d\n",
+            lutsize[0],
+            lutsize[1],
+            lutsize[2]);
+
+    lut = malloc(lutsize[0] * lutsize[1] * lutsize[2] * 3 * sizeof(float));
+    fillcargo.lut = lut;
+    fillcargo.s1 = lutsize[0];
+    fillcargo.s2 = lutsize[1];
+    fillcargo.s3 = lutsize[2];
+    cmsStageSampleCLutFloat(clutstage, lutFiller, &fillcargo, SAMPLER_INSPECT);
+
+
+
     for (int c = 0; c < 3; c++ )
     {
         free(outputtable[c]);
     }
+    free(lut);
 
     cmsCloseProfile(hProfile);
 
